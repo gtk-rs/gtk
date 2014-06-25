9ef80970a0e320b8dc6e5fbf68e5483de9482e4e
JeremyLetang <letang.jeremy@gmail.com>
2014-06-25 13:15:15 +0200

    begin work on glibs list types

--- src/glib/mod.rs
+++ src/glib/mod.rs
@@ -15,6 +15,15 @@
 
 use libc::c_char;
 
+pub use self::list::List;
+pub use self::slist::SList;
+pub use self::glib_container::GlibContainer;
+
+mod list;
+mod slist;
+mod ffi;
+mod glib_container;
+
 // An opaque structure used as the base of all interface types.
 pub struct TypeInterface;
 
