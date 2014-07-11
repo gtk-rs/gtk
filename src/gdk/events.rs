42ba835fd82d80b59189be07cb4c739d440da3cc
GuillaumeGomez <guillaume1.gomez@gmail.com>
2014-07-11 13:16:02 +0200

    New functions in Widget

--- src/gdk/events.rs
+++ src/gdk/events.rs
@@ -1,62 +1,61 @@
-
 use gdk;
 use libc::c_void;
 use std::mem;
 
-pub enum EventType{
-  Nothing          = -1,
-  Delete           = 0,
-  Destroy          = 1,
-  Expose           = 2,
-  MotionNotify     = 3,
-  ButtonPress      = 4,
+pub enum EventType {
+  Nothing           = -1,
+  Delete            = 0,
+  Destroy           = 1,
+  Expose            = 2,
+  MotionNotify      = 3,
+  ButtonPress       = 4,
   DoubleButtonPress = 5,
   TripleButtonPress = 6,
-  ButtonRelease    = 7,
-  KeyPress         = 8,
-  KeyRelease       = 9,
-  EnterNotify      = 10,
-  LeaveNotify      = 11,
-  FocusChange      = 12,
-  Configure        = 13,
-  Map              = 14,
-  Unmap            = 15,
-  PropertyNotify   = 16,
-  SelectionClear   = 17,
-  SelectionRequest = 18,
-  SelectionNotify  = 19,
-  ProximityIn      = 20,
-  ProximityOut     = 21,
-  DragEnter        = 22,
-  DragLeave        = 23,
-  DragMotion       = 24,
-  DragStatus       = 25,
-  DropStart        = 26,
-  DropFinished     = 27,
-  ClientEvent      = 28,
-  VisibilityNotify = 29,
-  Scroll           = 31,
-  WindowState      = 32,
-  Setting          = 33,
-  OwnerChange      = 34,
-  GrabBroken       = 35,
-  Damage           = 36,
-  TouchBegin       = 37,
-  TouchUpdate      = 38,
-  TouchEnd         = 39,
-  TouchCancel      = 40
+  ButtonRelease     = 7,
+  KeyPress          = 8,
+  KeyRelease        = 9,
+  EnterNotify       = 10,
+  LeaveNotify       = 11,
+  FocusChange       = 12,
+  Configure         = 13,
+  Map               = 14,
+  Unmap             = 15,
+  PropertyNotify    = 16,
+  SelectionClear    = 17,
+  SelectionRequest  = 18,
+  SelectionNotify   = 19,
+  ProximityIn       = 20,
+  ProximityOut      = 21,
+  DragEnter         = 22,
+  DragLeave         = 23,
+  DragMotion        = 24,
+  DragStatus        = 25,
+  DropStart         = 26,
+  DropFinished      = 27,
+  ClientEvent       = 28,
+  VisibilityNotify  = 29,
+  Scroll            = 31,
+  WindowState       = 32,
+  Setting           = 33,
+  OwnerChange       = 34,
+  GrabBroken        = 35,
+  Damage            = 36,
+  TouchBegin        = 37,
+  TouchUpdate       = 38,
+  TouchEnd          = 39,
+  TouchCancel       = 40
 }
 
 pub trait Event {
-  fn get_send_event(&self) -> bool{
-    unsafe{
+  fn get_send_event(&self) -> bool {
+    unsafe {
       let event_any : &EventAny = mem::transmute(self);
       event_any.send_event == 0
     }
   }
 }
 
-pub struct EventAny{
+pub struct EventAny {
   pub _type : gdk::EventType,
   pub window : *mut gdk::Window,
   send_event : i8,
