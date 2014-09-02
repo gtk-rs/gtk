826975c2e118cdc13c15c20298d7767c2010f8e9
dndanik <dndanik@gmail.com>
2014-09-02 17:44:39 +0000

    Add GApp* support

--- gtk_glue/gtk_glue.c
+++ gtk_glue/gtk_glue.c
@@ -462,13 +462,13 @@ GtkActionBar* cast_GtkActionBar(GtkWidget* widget) {
 
 #endif
 
-/*GAppInfo* cast_GtkAppInfo(GtkWidget* widget) {
+GAppInfo* cast_GtkAppInfo(GtkWidget* widget) {
     return G_APP_INFO(widget);
 }
 
 GAppLaunchContext* cast_GtkAppLaunchContext(GtkWidget* widget) {
     return G_APP_LAUNCH_CONTEXT(widget);
-}*/
+}
 
 /* MAC OS dylib
 gcc -I/usr/local/include/gtk-3.0 -I/usr/local/include/glib-2.0 -I/usr/local/include/gobject-introspection-1.0 -I/usr/local/Cellar/glib/2.38.1/lib/glib-2.0/include/ -I/usr/local/Cellar/pango/1.36.0/include/pango-1.0/ -I/usr/local/Cellar/cairo/1.12.16/include/cairo/ -I/usr/local/Cellar/gdk-pixbuf/2.30.0/include/gdk-pixbuf-2.0/ -I/usr/local/Cellar/atk/2.10.0/include/atk-1.0/ -lglib-2.0 -lgtk-3.0 -lgobject-2.0 -dynamiclib -o libgtk_glue.dylib -dy gtk_glue.c
