6e903ffd1ab38a89ac1f1a8f9a25c7689ad477ff
Sebastian Schulze <me@bstr.eu>
2014-10-16 17:45:24 +0200

    get GTK_VERSION from pkg-config

--- cargobuild.sh
+++ cargobuild.sh
@@ -1,6 +1,6 @@
 set +e
 
 ./configure
-export GTK_VERSION=${GTK_VERSION:=GTK_3_12}
+export GTK_VERSION=${GTK_VERSION:=$(pkg-config --modversion gtk+-3.0 | awk -F. '{print "GTK_3_"$2}')}
 echo $GTK_VERSION
 make glue
