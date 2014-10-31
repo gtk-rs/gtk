98cdfa8fdba63fb64fffea80436d6aea6dac167f
GuillaumeGomez <guillaume1.gomez@gmail.com>
2014-10-31 17:07:30 +0100

    Add widget's description

--- src/gtk/widgets/placessidebar.rs
+++ src/gtk/widgets/placessidebar.rs
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with rgtk.  If not, see <http://www.gnu.org/licenses/>.
 
+//! GtkPlacesSidebar — Sidebar that displays frequently-used places in the file system
+
 use gtk::ffi;
 use gtk::ffi::FFIWidget;
 use gtk::traits;
