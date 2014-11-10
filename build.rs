8b09717db1a82fda5ad2ef0ed70333b2f335bc51
GuillaumeGomez <guillaume1.gomez@gmail.com>
2014-11-10 23:33:25 +0100

    Improve cargo build script

--- build.rs
+++ build.rs
@@ -14,7 +14,35 @@
 // along with rgtk.  If not, see <http://www.gnu.org/licenses/>.
 
 use std::io::Command;
+use std::os;
+use std::str;
 
 fn main() {
-    Command::new("sh").arg("cargobuild.sh").status().unwrap();
+    Command::new("./configure").status().unwrap();
+    if os::getenv(format!("{}_NO_PKG_CONFIG", envify("gtk+-3.0")).as_slice()).is_some() {
+        panic!(format!("pkg-config requested to be aborted for {}", "gtk+-3.0"))
+    }
+    let mut cmd = Command::new("pkg-config");
+    
+    cmd.arg("--modversion").arg("gtk+-3.0");
+
+    let out = match cmd.output() {
+        Ok(s) => s,
+        Err(e) => panic!(e)
+    };
+
+    let stdout : Vec<&str> = str::from_utf8(out.output.as_slice()).unwrap().split_str(".").collect();
+
+    let gtk_version = format!("GTK_{}_{}", stdout[0], stdout[1]);
+    os::setenv("GTK_VERSION", gtk_version);
+
+    match Command::new("make").arg("glue").spawn() {
+        Ok(_) => {}
+        Err(e) => panic!(e)
+    }
+}
+
+fn envify(name: &str) -> String {
+    name.chars().map(|c| c.to_uppercase()).map(|c| if c == '-' {'_'} else {c})
+        .collect()
 }
