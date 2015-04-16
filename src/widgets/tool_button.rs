992c26c26933aea70bad9798edab129effcbc542
Gleb Kozyrev <gleb@gkoz.com>
2015-04-16 20:02:47 +0300

    Update the license headers

--- src/widgets/tool_button.rs
+++ src/widgets/tool_button.rs
@@ -1,17 +1,6 @@
-// This file is part of rgtk.
-//
-// rgtk is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// rgtk is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with rgtk.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright 2013-2015, The Rust-GNOME Project Developers.
+// See the COPYRIGHT file at the top-level directory of this distribution.
+// Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
 
 //! A ToolItem subclass that displays buttons
 
