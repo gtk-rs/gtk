309c43370cd9391fe6c6091d5a2d8fbef215f169
Evgeny Pashkin <epashkin@cognitive.ru>
2015-06-20 19:40:25 +0300

    Remove deprecated functions in ColorButton

--- src/widgets/color_button.rs
+++ src/widgets/color_button.rs
@@ -40,48 +40,10 @@ impl ColorButton {
         }
     }
     /// Creates a new color button.
-    /// Deprecated: since 3.4, use `new_with_rgba`
-    pub fn new_with_color(color: &gdk::Color) -> ColorButton {
-        unsafe {
-            Widget::from_glib_none(ffi::gtk_color_button_new_with_color(color))
-                .downcast_unchecked()
-        }
-    }
-    /// Creates a new color button.
     pub fn new_with_rgba(rgba: &gdk_ffi::GdkRGBA) -> ColorButton {
         unsafe {
             Widget::from_glib_none(ffi::gtk_color_button_new_with_rgba(rgba))
                 .downcast_unchecked()
-        }
-    }
-    /// Sets the current color to be `color`.
-    /// Deprecated: since 3.4, use `set_rgba`
-    pub fn set_color(&self, color: &gdk::Color) {
-        unsafe {
-            ffi::gtk_color_button_set_color(self.to_glib_none().0, color)
-        }
-    }
-    /// Sets `color` to be the current color in the ColorButton widget.
-    /// Deprecated: since 3.4, use `get_rgba`
-    pub fn get_color(&self) -> gdk::Color {
-        let color = gdk::Color { pixel: 0, red: 0, green: 0, blue: 0 };
-        unsafe {
-            ffi::gtk_color_button_get_color(self.to_glib_none().0, &color);
-        }
-        color
-    }
-    /// Sets the current opacity to be `alpha`.
-    /// Deprecated: since 3.4, use `set_rgba`
-    pub fn set_alpha(&self, alpha: u16) {
-        unsafe {
-            ffi::gtk_color_button_set_alpha(self.to_glib_none().0, alpha)
-        }
-    }
-    /// Returns the current alpha value.
-    /// Deprecated: since 3.4, use `get_rgba`
-    pub fn get_alpha(&self) -> u16 {
-        unsafe {
-            ffi::gtk_color_button_get_alpha(self.to_glib_none().0)
         }
     }
     /// Sets the title for the color selection dialog.
