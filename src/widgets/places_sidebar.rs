15f6a508584c0a06f419c3a9494cf86456a0661f
Guillaume Gomez <guillaume1.gomez@gmail.com>
2015-08-18 09:33:14 +0200

    Set good version

--- src/widgets/places_sidebar.rs
+++ src/widgets/places_sidebar.rs
@@ -52,13 +52,13 @@ impl PlacesSidebar {
         unsafe { to_bool(ffi::gtk_places_sidebar_get_local_only(GTK_PLACES_SIDEBAR(self.unwrap_widget()))) }
     }
 
-    #[cfg(gtk_3_12)]
+    #[cfg(gtk_3_14)]
     pub fn set_show_enter_location(&self, show_enter_location: bool) {
         unsafe { ffi::gtk_places_sidebar_set_show_enter_location(GTK_PLACES_SIDEBAR(self.unwrap_widget()),
             to_gboolean(show_enter_location)) }
     }
 
-    #[cfg(gtk_3_12)]
+    #[cfg(gtk_3_14)]
     pub fn get_show_enter_location(&self) -> bool {
         unsafe { to_bool(ffi::gtk_places_sidebar_get_show_enter_location(GTK_PLACES_SIDEBAR(self.unwrap_widget()))) }
     }
