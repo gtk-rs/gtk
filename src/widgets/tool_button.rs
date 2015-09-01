139228cd201ba1548ebb1e3db1556dc76e08ef07
Gleb Kozyrev <gleb@gkoz.com>
2015-09-01 21:09:05 +0300

    Update the project name s/Rust-GNOME/Gtk-rs/

--- src/widgets/tool_button.rs
+++ src/widgets/tool_button.rs
@@ -1,4 +1,4 @@
-// Copyright 2013-2015, The Rust-GNOME Project Developers.
+// Copyright 2013-2015, The Gtk-rs Project Developers.
 // See the COPYRIGHT file at the top-level directory of this distribution.
 // Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
 
