165b15c165bff5c794acff7e4ded56d647f9f7a8
Gleb Kozyrev <gleb@gkoz.com>
2015-11-25 18:08:02 +0200

    ToGlibPtr signature change

--- src/chooser/recent.rs
+++ src/chooser/recent.rs
@@ -27,19 +27,18 @@ pub struct RecentData {
     is_private: bool
 }
 
-impl <'a> ToGlibPtr<'a, *mut ffi::GtkRecentData> for &'a RecentData {
+impl <'a> ToGlibPtr<'a, *mut ffi::GtkRecentData> for RecentData {
     type Storage = (Box<ffi::GtkRecentData>,
                     [Stash<'a, *const c_char, String>; 5],
-                    Stash<'a, *mut *const c_char, &'a [String]>);
+                    Stash<'a, *mut *const c_char, [String]>);
 
-    fn to_glib_none(&self)
-        -> Stash<'a, *mut ffi::GtkRecentData, &'a RecentData> {
+    fn to_glib_none(&'a self) -> Stash<'a, *mut ffi::GtkRecentData, Self> {
         let display_name = self.display_name.to_glib_none();
         let description = self.description.to_glib_none();
         let mime_type = self.mime_type.to_glib_none();
         let app_name = self.app_name.to_glib_none();
         let app_exec = self.app_exec.to_glib_none();
-        let groups = (&*self.groups).to_glib_none();
+        let groups = self.groups.to_glib_none();
 
         let mut data = Box::new(ffi::GtkRecentData {
             display_name: display_name.0 as *mut c_char,
