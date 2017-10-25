3a18a7c619ae271b0f0709b591d21ae80987822c
PizzaIter <pizzaiter@gmail.com>
2017-10-25 11:48:47 +0200

    Add fn join_group to RadioToolButton

--- src/radio_tool_button.rs
+++ src/radio_tool_button.rs
@@ -5,6 +5,8 @@ use ToolItem;
 use ffi;
 use glib::object::Downcast;
 use glib::translate::*;
+use glib::Value;
+use gobject_ffi;
 
 impl RadioToolButton {
     pub fn new() -> RadioToolButton {
@@ -18,6 +20,13 @@ impl RadioToolButton {
         assert_initialized_main_thread!();
         unsafe {
             ToolItem::from_glib_none(ffi::gtk_radio_tool_button_new_from_stock(ptr::null_mut(), stock_id.to_glib_none().0)).downcast_unchecked()
+        }
+    }
+
+    pub fn join_group<'a, P: Into<Option<&'a RadioToolButton>>>(&self, group: P) {
+        let group = group.into();
+        unsafe {
+            gobject_ffi::g_object_set_property(self.to_glib_none().0, "group".to_glib_none().0, Value::from(group).to_glib_none().0);
         }
     }
 }
