0194a0e930a41ba693b7132d04fdfd27d59bac6d
Xiang Fan <sfanxiang@gmail.com>
2019-03-08 10:58:47 +0800

    builder: Move trait methods to traits

--- src/builder.rs
+++ src/builder.rs
@@ -16,19 +16,26 @@ impl Builder {
         assert_initialized_main_thread!();
         unsafe { from_glib_full(ffi::gtk_builder_new_from_file(file_path.as_ref().to_glib_none().0)) }
     }
+}
 
-    pub fn get_object<T: IsA<Object>>(&self, name: &str) -> Option<T> {
+pub trait BuilderExtManual: 'static {
+    fn get_object<T: IsA<Object>>(&self, name: &str) -> Option<T>;
+    fn add_from_file<T: AsRef<Path>>(&self, file_path: T) -> Result<(), Error>;
+}
+
+impl<O: IsA<Builder>> BuilderExtManual for O {
+    fn get_object<T: IsA<Object>>(&self, name: &str) -> Option<T> {
         unsafe {
             Option::<Object>::from_glib_none(
-                ffi::gtk_builder_get_object(self.to_glib_none().0, name.to_glib_none().0))
+                ffi::gtk_builder_get_object(self.upcast_ref().to_glib_none().0, name.to_glib_none().0))
                 .and_then(|obj| obj.dynamic_cast::<T>().ok())
         }
     }
 
-    pub fn add_from_file<T: AsRef<Path>>(&self, file_path: T) -> Result<(), Error> {
+    fn add_from_file<T: AsRef<Path>>(&self, file_path: T) -> Result<(), Error> {
         unsafe {
             let mut error = ::std::ptr::null_mut();
-            ffi::gtk_builder_add_from_file(self.to_glib_none().0,
+            ffi::gtk_builder_add_from_file(self.upcast_ref().to_glib_none().0,
                                            file_path.as_ref().to_glib_none().0,
                                            &mut error);
             if error.is_null() { Ok(()) } else { Err(from_glib_full(error)) }
